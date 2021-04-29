b9d71daee3339a0d7de2ceab16ceb8c81e48fa6a
stzn <sztk1209@gmail.com>
2021-04-29 17:47:40 +0900

    change method order

--- FeedStoreChallenge/ManagedCache.swift
+++ FeedStoreChallenge/ManagedCache.swift
@@ -21,12 +21,12 @@ extension ManagedCache {
 		return try context.fetch(request).first
 	}
 
-	private static func deleteCache(in context: NSManagedObjectContext) throws {
-		try find(in: context).map(context.delete).map(context.save)
-	}
-
 	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
 		try deleteCache(in: context)
 		return ManagedCache(context: context)
+	}
+
+	private static func deleteCache(in context: NSManagedObjectContext) throws {
+		try find(in: context).map(context.delete).map(context.save)
 	}
 }
